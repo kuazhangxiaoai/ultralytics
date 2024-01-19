ae95c58cec28e1b17e0da8c0c57a2ada968535cd
kuazhangxiaoai <yanggang3510@gmail.com>
2024-01-19 16:55:29 +0800

    2024-1-19

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -1041,14 +1041,14 @@ def v8_transforms(dataset, imgsz, hyp, stretch=False):
         [
             Mosaic(dataset, imgsz=imgsz, p=hyp.mosaic),
             CopyPaste(p=hyp.copy_paste),
-            RandomPerspective(
-                degrees=hyp.degrees,
-                translate=hyp.translate,
-                scale=hyp.scale,
-                shear=hyp.shear,
-                perspective=hyp.perspective,
-                pre_transform=None if stretch else LetterBox(new_shape=(imgsz, imgsz)),
-            ),
+            #RandomPerspective(
+            #    degrees=hyp.degrees,
+            #    translate=hyp.translate,
+            #    scale=hyp.scale,
+            #    shear=hyp.shear,
+            #    perspective=hyp.perspective,
+            #    pre_transform=None if stretch else LetterBox(new_shape=(imgsz, imgsz)),
+            #),
         ]
     )
     flip_idx = dataset.data.get("flip_idx", [])  # for keypoints augmentation
